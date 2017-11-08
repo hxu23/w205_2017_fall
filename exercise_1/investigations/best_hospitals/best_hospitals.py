cac32cbad02d2b45626bd4cbe73f931fc4086c96
hxu23 <31424792+hxu23@users.noreply.github.com>
2017-11-07 21:29:24 -0500

    best_hospitals script

--- exercise_1/investigations/best_hospitals/best_hospitals.py
+++ exercise_1/investigations/best_hospitals/best_hospitals.py
@@ -18,13 +18,24 @@ hospitals = hc.sql('select * from hospitals_reduced').toPandas()
 effective_care = hc.sql('select * from effective_care_scores').toPandas()
 
 
-def find_best_hospital_overal():
-    df = pd.merge(hospitals, effective_care, how='inner', left_on='provider_id', right_on='provider_id')
-    df['hospital_overall_rating'] = df['hospital_overall_rating'].replace('Not Available').apply(pd.to_numeric)
+def best_hospital_overall():
+    df = pd.merge(hospitals, effective_care, how='inner', left_on='provider_id', right_on='provider_id') # merge effective_care_scores and hospitals_reduced
+    df['hospital_overall_rating'] = df['hospital_overall_rating'].replace('Not Available').apply(pd.to_numeric) # replace string values with numeric and
 
     print (df.groupby(['provider_id', 'hospital_name']).mean()[['hospital_overall_rating']]
-                                                       .sort_values(by='hospital_overall_rating', ascending=False)).head(10)
+                                                       .sort_values(by='hospital_overall_rating',
+                                                                    ascending=False)).head(10)
+    return
+
+def best_hospital_by_measures():
+    df = pd.merge(hospitals, effective_care, how='inner', left_on='provider_id', right_on='provider_id')
+    results = df.set_index(['provider_id', 'hospital_name', 'measure_id'])[['score']].unstack() # index by hospital and measure_id
+    results = results.apply(pd.to_numeric) # convert values to numeric
+    results.loc[:, 'Average_Score'] = results.mean(axis=1) # average score across all measures
+    results.loc[:, 'StDev_Score'] = results.std(axis=1) # standard deviation of scores across all measures
+    print results.sort_values(by='Average_Score', ascending=False).head(10)
     return
 
 if __name__ == '__main__':
-    find_best_hospital_overal()
+    best_hospital_overall()
+    best_hospital_by_measures()

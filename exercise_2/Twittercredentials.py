4179d75b41888b01dd63a03841a1df04f73d8f82
harry <harryxu@berkeley.edu>
2017-12-11 04:06:32 +0000

    updated

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "dU8wK7jFUSNEzaprvDDe6EAuI";
+consumer_key = "";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "DGZ0n2X5uoBD0yVQb5nedsDlfMORvgBtTFTmAdmvOOjrbjIlsP";
+consumer_secret = "";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "3013773773-LfrtQ9b1I4t87M41xHnq1j7ldvOP1JNl7zNG5EN";
+access_token = "";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "VY2QZqSAuyS1ASuwAYpDpz3oRpeZm7PpeW7Zl1st32xBe";
+acess_token_secret = "";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
